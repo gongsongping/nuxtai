700c9baa13d625213ed07b7a241d98714b5733d9
gongsongping <gongsongping@gmail.com>
2018-07-08 22:29:03 +0800

    veux-test

---
+++ pages/vx.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>
+      <button @click="increment" style='width:50px;height:30px;background:tomato;'>{{ counter }}</button><br>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  // fetch(context) is called by the server-side
+  // and nuxt before instantiating the component
+  fetch({ store }) {
+    store.commit("increment");
+  },
+  computed: mapState(["counter"]),
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
---
+++ store/index.js
@@ -0,0 +1,23 @@
+import Vuex from 'vuex'
+
+// import mutations from './mutations'
+
+const mutations = {
+  increment(state) {
+    state.counter++
+  }
+}
+
+// export default mutations
+
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      counter: 0
+    },
+    mutations
+  })
+}
+
+export default createStore
